27f0403da6699b71086831bf3bc3f873127ff845
vincentheddesheimer <86489953+vincentheddesheimer@users.noreply.github.com>
2023-06-08 18:16:47 +0200

    Update 00_replication_kang16.R

--- 00_replication_kang16.R
+++ 00_replication_kang16.R
@@ -11,3 +11,4 @@ bills <- read_dta("~/Dropbox (Princeton)/BBH/BBH1/data/Kang (2016) replication/D
 lobbying <- read_dta("~/Dropbox (Princeton)/BBH/BBH1/data/Kang (2016) replication/Datafortheweb/IntermediateDatasets/Lobbying/lobbyingbills_cleaned.dta")
 
 entity <- read_dta("~/Dropbox (Princeton)/BBH/BBH1/data/Kang (2016) replication/Datafortheweb/IntermediateDatasets/Lobbying/sdata110th_client.dta")
+
