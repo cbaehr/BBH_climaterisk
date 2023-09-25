52d1c51e3e6b730ad8c8ff241cf44d30b2196dfe
cbaehr <chjbaehr@gmail.com>
2023-09-25 13:45:45 -0400

    fix normalization bug

--- build/04_normalize_variables.R
+++ build/04_normalize_variables.R
@@ -12,7 +12,8 @@ if(Sys.info()["user"]=="vincentheddesheimer" ) {setwd("~/Dropbox (Princeton)/BBH
 pacman::p_load(data.table, tidyverse)
 
 #Load data
-df <- fread("data/03_final/lobbying_df_wide_reduced_normal.csv")
+df <- fread("data/03_final/lobbying_df_wide_reduced.csv") |>
+  data.frame()
 
 
 #Normalize variables for interpretation 
@@ -21,7 +22,7 @@ df <- fread("data/03_final/lobbying_df_wide_reduced_normal.csv")
 df_wide_cont_vars <- c("cc_expo_ew_y", "cc_expo_ew_q", "op_expo_ew_y", "rg_expo_ew_y", "ph_expo_ew_y",
                        "ebit", "at", "total_lobby")
 ## pull from main data
-df_wide_cont <- df[, ..df_wide_cont_vars]
+df_wide_cont <- df[, df_wide_cont_vars]
 ## rescale to standard normal
 df_wide_cont <- scale(df_wide_cont)
 ## slot back into main df_wide
