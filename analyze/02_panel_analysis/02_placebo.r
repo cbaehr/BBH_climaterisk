7d97e4a7a3d57ef8ef5e8ae47b17967ebfd9d9f5
fbare <fiona.bare@gmail.com>
2023-09-25 20:01:15 -0400

    minor updates to placebo

--- analyze/02_panel_analysis/02_placebo.r
+++ analyze/02_panel_analysis/02_placebo.r
@@ -19,8 +19,72 @@ if(Sys.info()["user"]=="christianbaehr" ) {setwd("/Users/christianbaehr/Dropbox/
 if(Sys.info()["user"]=="vincentheddesheimer" ) {setwd("~/Dropbox (Princeton)/BBH/BBH1/")}
 if(Sys.info()["user"]=="fiona" ) {setwd("/Users/Dropbox (Princeton)/BBH/BBH1/")}
 
+##Placebo analysis 
+#Load normalized dataset
+df <- fread("data/03_final/lobbying_df_wide_reduced_normal.csv")
 
-###Placebo exploration 
+
+#Create new variable that is ebit/assets
+df$ebit_at <- df$ebit / df$at
+
+# Specify covariate names
+cm <- c("op_expo_ew_y" = "Opportunity Exposure",
+        "rg_expo_ew_y" = "Regulatory Exposure",
+        "ph_expo_ew_y" = "Physical Exposure", 
+        "cc_expo_ew_y" = "Overall Exposure", 
+        "ebit" = "EBIT",
+        "ebit_at" = "EBIT/Assets",
+        "us_dummy" = "US HQ",
+        "total_lobby" = "Total Lobbying ($)"
+)
+
+#Run model for first issue code (agriculture) - this works
+ag <- feglm(AGR ~ op_expo_ew_y + rg_expo_ew_y + ph_expo_ew_y + ebit + ebit_at + us_dummy + total_lobby| year + industry + industry_year, family = "binomial", df)
+
+############
+##Identify placebo variables
+# Specify columns to remove
+columns_to_remove <- c("CAW", "ENV", "ENG", "FUE")
+
+# Remove specified columns
+df_placebo <- df |>
+  select(-all_of(columns_to_remove))
+
+start_index <- which(names(df_placebo) == "AGR")
+end_index <- which(names(df_placebo) == "REL")
+
+# Extract column names between "AGR" and "REL" into a character vector
+dependent_vars <- names(df_placebo)[start_index:end_index]
+
+# View the character vector of column names
+print(dependent_vars)
+
+# Initialize an empty list to store model results
+results_list <- list()
+
+# Iterate over the dependent variables and fit the feglm model with fixed effects for each
+
+for (dv in dependent_vars) { {
+  formula <- as.formula(paste(dv, "~ op_expo_ew_y + rg_expo_ew_y + ph_expo_ew_y + ebit + ebit_at + us_dummy + total_lobby| year + industry + industry_year")) # Include fixed effects in the formula
+  
+  # Fit the feglm model
+  model <- feglm(formula, family = "binomial", data = df)
+  
+  # Store the model results in the list
+  results_list[[paste(dv, sep = "_")]] <- summary(model)
+}
+}
+
+# Access the results for each dependent variable and fixed effect combination
+for (dv in dependent_vars) {
+  cat("Model results for", dv, ":\n")
+  print(results_list[[paste(dv, sep = "_")]])
+}
+
+
+############################
+
+###Placebo exploration - Vincent's old code
 #load dataset
 df <- fread("data/lobbying_df_w_directionality.csv") |> 
   mutate(
@@ -83,74 +147,3 @@ df %>%
 ggsave("results/Figures/descriptives/expenditure_by_issue_code.pdf", width = 9, height = 13)
 
 
-##Placebo analysis 
-#Load dataset
-df <- fread("data/03_final/lobbying_df_wide_reduced.csv")
-
-#Normalize variables for interpretation 
-## continuous variables in regression models
-df_wide_cont_vars <- c("cc_expo_ew_y", "cc_expo_ew_q", "op_expo_ew_y", "rg_expo_ew_y", "ph_expo_ew_y",
-                       "ebit", "at", "total_lobby")
-## pull from main data
-df_wide_cont <- df[, ..df_wide_cont_vars]
-## rescale to standard normal
-df_wide_cont <- scale(df_wide_cont)
-## slot back into main df_wide
-df[, df_wide_cont_vars] <- df_wide_cont
-
-#Create new variable that is ebit/assets
-df$ebit_at <- df$ebit / df$at
-
-# Specify covariate names
-cm <- c("op_expo_ew_y" = "Opportunity Exposure",
-        "rg_expo_ew_y" = "Regulatory Exposure",
-        "ph_expo_ew_y" = "Physical Exposure", 
-        "cc_expo_ew_y" = "Overall Exposure", 
-        "ebit" = "EBIT",
-        "ebit_at" = "EBIT/Assets",
-        "us_dummy" = "US HQ",
-        "total_lobby" = "Total Lobbying ($)"
-)
-
-#Run model for first issue code (agriculture) - this works
-ag <- feglm(AGR ~ op_expo_ew_y + rg_expo_ew_y + ph_expo_ew_y + ebit + ebit_at + us_dummy + total_lobby| year + industry + industry_year, family = "binomial", df)
-
-############
-##Identify placebo variables
-# Specify columns to remove
-columns_to_remove <- c("CAW", "ENV", "ENG", "FUE")
-
-# Remove specified columns
-df_placebo <- df |>
-  select(-all_of(columns_to_remove))
-
-start_index <- which(names(df_placebo) == "AGR")
-end_index <- which(names(df_placebo) == "REL")
-
-# Extract column names between "AGR" and "REL" into a character vector
-dependent_vars <- names(df_placebo)[start_index:end_index]
-
-# View the character vector of column names
-print(dependent_vars)
-
-# Initialize an empty list to store model results
-results_list <- list()
-
-# Iterate over the dependent variables and fit the feglm model with fixed effects for each
-
-for (dv in dependent_vars) { {
-    formula <- as.formula(paste(dv, "~ op_expo_ew_y + rg_expo_ew_y + ph_expo_ew_y + ebit + ebit_at + us_dummy + total_lobby| year + industry + industry_year")) # Include fixed effects in the formula
-    
-    # Fit the feglm model
-    model <- feglm(formula, family = "binomial", data = df)
-    
-    # Store the model results in the list
-    results_list[[paste(dv, sep = "_")]] <- summary(model)
-  }
-}
-
-# Access the results for each dependent variable and fixed effect combination
-for (dv in dependent_vars) {
-    cat("Model results for", dv, ":\n")
-    print(results_list[[paste(dv, sep = "_")]])
-  }
