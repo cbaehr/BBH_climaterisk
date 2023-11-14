980a6f1c33b5dd5c461c6ac79417b55427a5c869
vincentheddesheimer <86489953+vincentheddesheimer@users.noreply.github.com>
2023-11-13 22:36:52 -0500

    Add diagnostic checks

--- build/06_data_quality_checks.R
+++ build/06_data_quality_checks.R
@@ -6,6 +6,8 @@ pacman::p_load(tidyverse, data.table, modelsummary)
 options(scipen=999)
 
 if(Sys.info()["user"]=="christianbaehr" ) {setwd("/Users/christianbaehr/Dropbox/BBH/BBH1/")}
+if(Sys.info()["user"]=="vincentheddesheimer" ) {setwd("~/Dropbox (Princeton)/BBH/BBH1/")}
+
 df <- read.csv("data/03_final/lobbying_df_annual_REVISE_normal.csv", stringsAsFactors = F)
 
 df$ph_expo_ew <- as.numeric(df$ph_expo_ew)
@@ -48,4 +50,76 @@ df$at[which(df$conm=="HUNTSMAN CORPORATION")]
 ## over which we omit the row from the data or censor the value at 2-standard deviations.
 ## similar to Winsorizing
 
+# @Christian: I agree, that sounds reasonable
 
+
+
+# Diagnose with dlookr ----------------------------------------------------
+
+pacman::p_load(dlookr)
+
+# simple diagnose
+diag_df <- diagnose(df)
+
+# check missing: plot top 15 variables with most missing values
+missingness <- df %>%
+  diagnose() %>%
+  select(-unique_count, -unique_rate) %>% 
+  filter(missing_count > 0) %>% 
+  arrange(desc(missing_count))
+# 90% missingness lobbying
+# 60% missingness exposure
+# 40% missingness orbis
+
+# diagnose numeric
+numerics <- df %>%
+  select_if(is.numeric) %>%
+  diagnose_numeric()
+
+# diagnose outliers
+outliers <- df %>%
+  select_if(is.numeric) %>%
+  diagnose_outlier()
+
+# get variables with most outliers
+outliers %>%
+  arrange(desc(outliers_cnt)) %>%
+  head(30)
+# ebit, ebit_at, and at have the most outliers
+# also total_lobby_annual
+hist(df$total_lobby_annual)
+summary(df$total_lobby_annual)
+
+# plot outliers for total_lobby_annual
+df %>%
+  plot_outlier(total_lobby_annual)
+
+# get highest 50 outliers
+df %>%
+  arrange(desc(total_lobby_annual)) %>%
+  head(50) %>%
+  select(conm, total_lobby_annual, year)
+# Seems reasonable
+
+## Look at CLI_amount_annual
+df %>%
+  plot_outlier(CLI_amount_annual)
+
+# get highest 50 outliers
+df %>%
+  arrange(desc(CLI_amount_annual)) %>%
+  head(50) %>%
+  select(conm, CLI_amount_annual, year)
+# Seems reasonable
+
+# Look at ebit/at
+summary(df$ebit_at)
+# Infinity
+
+# Get observations where ebit_at == Inf
+df %>%
+  filter(ebit_at == Inf) %>%
+  select(conm, ebit, at, ebit_at, year)
+# We should code at == 1 in these cases
+
+### END
