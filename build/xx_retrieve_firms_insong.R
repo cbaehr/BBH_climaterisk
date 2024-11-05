7f6d75bb9a1a699282a76de59dc06bad45607248
vincentheddesheimer <86489953+vincentheddesheimer@users.noreply.github.com>
2024-11-05 15:13:42 -0500

    Retrieve list of firms for In Song

---
+++ build/xx_retrieve_firms_insong.R
@@ -0,0 +1,36 @@
+# Script to retrieve firms that were exposed to climate and lobbied on specific bills
+
+rm(list=ls())
+
+# load packages
+pacman::p_load(tidyverse)
+
+# set working directory
+if(Sys.info()["user"]=="fiona" ) {setwd("/Users/fiona/Dropbox/BBH/BBH1/")}
+if(Sys.info()["user"]=="christianbaehr" ) {setwd("/Users/christianbaehr/Dropbox/BBH/BBH1/")}
+if(Sys.info()["user"]=="vincentheddesheimer" ) {setwd("~/Dropbox (Princeton)/BBH/BBH1/")}
+
+
+# load data
+df <- read_rds("data/03_final/lobbying_df_quarterly_REVISE_normal.rds")
+
+# Filter: Firms that were exposed to climate
+firms_exposed <- df %>%
+  filter(op_expo_ew > 0 | rg_expo_ew > 0 | ph_expo_ew > 0) %>%
+  distinct(isin)
+
+# Filter: Firms that lobbied on climate
+firms_lobby_CLI <- df %>%
+  filter(CLI_quarter > 0) %>%
+  distinct(isin)
+
+
+# Firms that were exposed to climate and lobbied on climate
+firms_exposed_lobby_CLI <- firms_exposed %>%
+  filter(isin %in% firms_lobby_CLI$isin) %>%
+  distinct(isin)
+
+# Save
+saveRDS(firms_exposed_lobby_CLI, "data/xx_other/firms_exposed_lobby_CLI.rds")
+
+### END
