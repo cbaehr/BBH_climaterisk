802d974aa8100350d6eb8279e18b10b7df9b0b91
cbaehr <chjbaehr@gmail.com>
2025-01-13 15:09:04 -0500

    tweak coalitions code

--- build/08_merge_w_coalitions.R
+++ build/08_merge_w_coalitions.R
@@ -15,21 +15,14 @@ if(Sys.info()["user"]=="vincentheddesheimer" ) {setwd("~/Dropbox (Princeton)/BBH
 
 # Load data ---------------------------------------------------------------
 
-df <- fread("data/lobbying_df_fb.csv")
-df_wide <- fread("data/lobbying_df_wide_reduced.csv")
+df <- read_rds("data/03_final/lobbying_df_annual_REVISE_normal.rds")
 
-coal <- fread("data/Lerner and Osgood 2022 replication/analysis_data_no_proprietary.csv")
-
+coal <- fread("data/01_raw/coalitions/Lerner and Osgood 2022 replication/analysis_data_no_proprietary.csv")
 
 
 # Edit coalition data ---------------------------------------------------
 
-coal <- coal |>
-  select(
-    gvkey, year,
-    numsupcoal,# coalitions joined that strongly or weakly favor climate action
-    numoppcoal # coalitions joined that strongly or weakly oppose climate action
-  ) 
+coal <- coal[ , c("gvkey", "year", "numsupcoal", "numoppcoal")]
 
 coal |> tabyl(numsupcoal)
 coal |> tabyl(numoppcoal)
@@ -56,24 +49,15 @@ coal |>
 coal |> tabyl(sup_climate_action, opp_climate_action) # not much overlap: 274 firm-years / 45 firms
 
 
-
-
 # Merge with df -----------------------------------------------------------
 
 df$year <- as.numeric(df$year)
+coal$gvkey <- as.integer(coal$gvkey)
 df2 <- df |>
   left_join(coal, by = c("gvkey", "year"))
 
 df2 |> tabyl(sup_climate_action)
 df2 |> tabyl(opp_climate_action)
-
-
-df_wide$year <- as.numeric(df_wide$year)
-
-df_wide2 <- df_wide |> 
-  mutate(gvkey = as.character(gvkey)) |>
-  left_join(coal, by = c("gvkey", "year"))
-
 
 # Code directionality -----------------------------------------------------
 
@@ -81,21 +65,10 @@ df_wide2 <- df_wide |>
 ## Long df --------------------------------------------------------------
 
 df3 <- df2 |>
+  mutate(CLI = ifelse(grepl("ENV|CAW|ENG|FUE", issue_code), 1, 0)) |>
+  mutate(pro_CLI = ifelse(CLI == 1 & sup_climate_action == 1 & opp_climate_action == 0, 1, 0), 
+         contra_CLI = ifelse(CLI == 1 & opp_climate_action == 1 & sup_climate_action == 0, 1, 0)) |>
   mutate(
-    # lobbying on climate
-    CLI = ifelse(issue_code %in% c("ENV", "CAW", "ENG", "FUE"),
-                 1,
-                 0),
-    pro_CLI = ifelse(CLI == 1 &
-                       sup_climate_action == 1 &
-                       opp_climate_action == 0,
-                     1,
-                     0),
-    contra_CLI = ifelse(CLI == 1 &
-                          opp_climate_action == 1 &
-                          sup_climate_action == 0,
-                        1,
-                        0),
     direction_CLI = case_when(
       pro_CLI == 1 ~ "Pro", 
       contra_CLI == 1 ~ "Contra",
@@ -103,65 +76,67 @@ df3 <- df2 |>
       CLI == 1 & sup_climate_action != 1 & opp_climate_action != 1 ~ "None")
   )
 
-insp <- df3 |> select(gvkey, year, issue_code, CLI, pro_CLI, direction_CLI, sup_climate_action, contra_CLI, opp_climate_action)
+#insp <- df3 |> select(gvkey, year, issue_code, CLI, pro_CLI, direction_CLI, sup_climate_action, contra_CLI, opp_climate_action)
+insp <- df3[ , c("gvkey", "year", "issue_code", "CLI", "pro_CLI", "direction_CLI", 
+                 "sup_climate_action", "contra_CLI", "opp_climate_action")]
 
 df3 |> tabyl(pro_CLI)
 df3 |> tabyl(contra_CLI)
 
 
 desc <- df3 |>
-  filter(CLI == 1) |>
-  select(gvkey, year, direction_CLI)
+  filter(CLI == 1)
+desc <- desc[ , c("gvkey", "year", "direction_CLI")]
 
-desc |> tabyl(direction_CLI) |>
-  adorn_pct_formatting() |>
-  select(-valid_percent) |>
-  rename(`Lobbying Direction` = direction_CLI,
-         N = n,
-         Percent = percent) |>
-  mutate(order = case_when(
-    `Lobbying Direction` == "Pro" ~ 1,
-    `Lobbying Direction` == "Contra" ~ 2,
-    `Lobbying Direction` == "Both" ~ 3,
-    `Lobbying Direction` == "None" ~ 4,
-    `Lobbying Direction` == "<NA>" ~ 5
-  )) |>
-  arrange(order) |>
-  select(-order) |>
-  kbl(booktabs = TRUE, "latex", caption = "Direction of Lobbying on Climate Issues",
-      label = "lobbying_direction_desc") |>
-  kable_styling(
-    latex_options = "hold_position"
-  )
+# desc |> tabyl(direction_CLI) |>
+#   adorn_pct_formatting() |>
+#   select(-valid_percent) |>
+#   rename(`Lobbying Direction` = direction_CLI,
+#          N = n,
+#          Percent = percent) |>
+#   mutate(order = case_when(
+#     `Lobbying Direction` == "Pro" ~ 1,
+#     `Lobbying Direction` == "Contra" ~ 2,
+#     `Lobbying Direction` == "Both" ~ 3,
+#     `Lobbying Direction` == "None" ~ 4,
+#     `Lobbying Direction` == "<NA>" ~ 5
+#   )) |>
+#   arrange(order) |>
+#   select(-order) |>
+#   kbl(booktabs = TRUE, "latex", caption = "Direction of Lobbying on Climate Issues",
+#       label = "lobbying_direction_desc") |>
+#   kable_styling(
+#     latex_options = "hold_position"
+#   )
   
 
 
 ## Wide df_reduced ---------------------------------------------------------
 
-df_wide3 <- df_wide2 |>
-  mutate(
-    # lobbying on climate
-    CLI = ifelse(ENV == 1 |
-                   CAW == 1 |
-                   ENG == 1 |
-                   FUE == 1,
-                 1, 0),
-    pro_CLI = ifelse(CLI == 1 &
-                       sup_climate_action == 1 &
-                       opp_climate_action == 0,
-                     1,
-                     0),
-    contra_CLI = ifelse(CLI == 1 &
-                          opp_climate_action == 1 &
-                          sup_climate_action == 0,
-                        1,
-                        0),
-    direction_CLI = case_when(
-      pro_CLI == 1 ~ "Pro", 
-      contra_CLI == 1 ~ "Contra",
-      CLI == 1 & sup_climate_action == 1 & opp_climate_action == 1 ~ "Both", 
-      CLI == 1 & sup_climate_action != 1 & opp_climate_action != 1 ~ "None")
-  )
+# df_wide3 <- df_wide2 |>
+#   mutate(
+#     # lobbying on climate
+#     CLI = ifelse(ENV == 1 |
+#                    CAW == 1 |
+#                    ENG == 1 |
+#                    FUE == 1,
+#                  1, 0),
+#     pro_CLI = ifelse(CLI == 1 &
+#                        sup_climate_action == 1 &
+#                        opp_climate_action == 0,
+#                      1,
+#                      0),
+#     contra_CLI = ifelse(CLI == 1 &
+#                           opp_climate_action == 1 &
+#                           sup_climate_action == 0,
+#                         1,
+#                         0),
+#     direction_CLI = case_when(
+#       pro_CLI == 1 ~ "Pro", 
+#       contra_CLI == 1 ~ "Contra",
+#       CLI == 1 & sup_climate_action == 1 & opp_climate_action == 1 ~ "Both", 
+#       CLI == 1 & sup_climate_action != 1 & opp_climate_action != 1 ~ "None")
+#   )
 
 
 
@@ -169,7 +144,7 @@ df_wide3 <- df_wide2 |>
 
 # Write -------------------------------------------------------------------
 
-fwrite(df3, "data/lobbying_df_w_directionality.csv")
-fwrite(df_wide3, "data/lobbying_df_wide__red_w_directionality.csv")
+fwrite(df3, "data/03_final/lobbying_df_w_directionality_REVISE.csv")
+#fwrite(df_wide3, "data/lobbying_df_wide__red_w_directionality.csv")
 
 ### END
