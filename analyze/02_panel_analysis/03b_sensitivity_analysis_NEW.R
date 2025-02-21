767a46a48a2e30e109de7066f62614f294935b4c
vincentheddesheimer <86489953+vincentheddesheimer@users.noreply.github.com>
2025-02-21 09:16:58 -0500

    descriptives, placebo, sensitivity

--- analyze/02_panel_analysis/03b_sensitivity_analysis_NEW.R
+++ analyze/02_panel_analysis/03b_sensitivity_analysis_NEW.R
@@ -3,10 +3,13 @@
 
 rm(list=ls())
 
+# no scientific notation
+options(scipen = 999)
+
 # devtools::install_github("chadhazlett/sensemakr")
 
 # Load packages
-pacman::p_load(data.table, tidyverse, fixest, sensemakr)
+pacman::p_load(data.table, tidyverse, fixest, sensemakr, kableExtra)
 
 
 # set working directory
@@ -16,40 +19,11 @@ if(Sys.info()["user"]=="vincentheddesheimer" ) {setwd("~/Dropbox (Princeton)/BBH
 
 
 # load data
-df <- read_rds("data/03_final/lobbying_df_quarterly_REVISE_normal.rds")
-
-# df_old <- read_rds("data/03_final/lobbying_df_quarterly_REVISE.rds")
+df <- read_rds("data/03_final/lobbying_df_quarterly_REVISE_normal_NEW.rds")
 
 names(df)
 
-
-# # Transform Variables -----------------------------------------------------
-
-# # dummy variable: climate issues
-# df <- df |>
-#   mutate(CLI = ifelse(ENV == 1 | 
-#                         CAW == 1 |
-#                         ENG == 1 |
-#                         FUE == 1,
-#                       1,0))
-
-# df$CLI_dollars <- apply(df[, c("amount_num_ENV", "amount_num_CAW", "amount_num_ENG", "amount_num_FUE")],
-#                         1, function(x) sum(x, na.rm=T) / 1000000)
-
-
-
-# Control variables -------------------------------------------------------
-
-# #US dummy variable 
-# df <- df |>
-#   mutate(us_dummy = ifelse(hqcountrycode == "US",1,0))
-
-# #Total annual lobbying (total dollars)
-# df <- df |>
-#   group_by(gvkey, year) |>
-#   # mutate(total_lobby = n_distinct(report_uuid))
-#   mutate(total_lobby = sum(c_across(grep("amount_num", names(df), value=T))))
-
+# create lag
 df <- df |>
   # group by unit (in our case: firm)
   group_by(isin) |>
@@ -57,45 +31,168 @@ df <- df |>
   arrange(yearqtr) |>
   # for one year
   mutate(#log_co2_l1 = lag(log_co2, 1),
-    CLI_lag = lag(CLI, 1)) |>
+    CLI_lag = lag(CLI_quarter, 1)) |>
   #ungroup
   ungroup()
 
-
-# df$industry <- df$bvd_sector
-# df <- df[which(df$industry!=""), ]
-# df$industry_year <- paste(df$industry, df$year)
-
 sum(duplicated(df[, c("yearqtr", "isin")]))
 
-# ## continuous variables in regression models
-# df_cont_vars <- c("cc_expo_ew", "cc_expo_ew", "op_expo_ew", "rg_expo_ew", "ph_expo_ew",
-#                   "ebit_scaled", "at_scaled", "total_lobby_quarter_scaled", "ebit_at_scaled")
-# ## pull from main data
-# df_cont <- df[, df_cont_vars]
-# ## rescale to standard normal
-# df_cont <- scale(df_cont)
-# ## slot back into main df
-# df[, df_cont_vars] <- df_cont
-
-
-# # Transform variabls for regression
-# df <- df |>
-#   mutate(
-#     ebit_asset = ebit/at,
-#     CLI_dollars = log(CLI_dollars +1)
-#   )
 
 
 # Run Models --------------------------------------------------------------
 
-# Occurrence --------------------------------------------------------------
-
-df$`Total Lobbying`<- df$total_lobby_quarter_scaled
 
 df$CLI <- as.numeric(df$CLI_quarter)
 
-m_occurrence <- feols(CLI ~ op_expo_ew + rg_expo_ew + ph_expo_ew + ebit_at_scaled + us_dummy_scaled + `Total Lobbying` | industry_year, df, vcov = ~ year + isin)
+df$log_CLI_amount <- log(df$CLI_amount_quarter +1)
+
+m_occurrence <- feols(CLI ~ op_expo_ew + rg_expo_ew + ph_expo_ew + ebit + ebit_at + us_dummy + total_lobby_quarter | industry_year, df, vcov = ~ year + isin)
+#m_occurrence_iid <- feols(CLI ~ op_expo_ew + rg_expo_ew + ph_expo_ew + ebit + ebit_at + us_dummy + total_lobby_quarter | industry_year, df, vcov = "iid")
+
+m_amount <- feols(log_CLI_amount ~ op_expo_ew + rg_expo_ew + ph_expo_ew + ebit + ebit_at + us_dummy + total_lobby_quarter | industry_year, df, vcov = ~ year + isin)
+
+# Get minimal statistics ---------------------------------------------------
+
+# # Some inspections
+# sense <- sensemakr(
+#   estimate = coef(m_occurrence)[["op_expo_ew"]],
+#   se = se(m_occurrence)[["op_expo_ew"]],
+#   dof = df.residual(m_occurrence),
+#   treatment = "op_expo_ew"
+# )
+
+# sense$sensitivity_stats
+
+# se(m_occurrence)["op_expo_ew"]
+# se(m_occurrence_iid)["op_expo_ew"]
+# clustering makes huge difference for ses and t-stats
+
+# Function to calculate sensitivity stats with clustered SEs
+get_sensitivity_stats_clustered <- function(model, treatment_var) {
+  # Create sensitivity analysis using manual inputs
+  sense <- sensemakr(
+    estimate = coef(model)[treatment_var],
+    se = se(model)[treatment_var],
+    dof = df.residual(model),
+    treatment = treatment_var
+  )
+  
+  # Extract stats and convert to data.frame
+  stats_df <- as.data.frame(sense$sensitivity_stats)
+  stats_df$model_type <- if(grepl("amount", deparse(substitute(model)))) "Expenditure" else "Occurrence"
+  
+  return(stats_df)
+}
+
+# Get stats for both models and all treatment variables
+sensitivity_results <- rbind(
+  get_sensitivity_stats_clustered(m_occurrence, "op_expo_ew"),
+  get_sensitivity_stats_clustered(m_occurrence, "rg_expo_ew"),
+  get_sensitivity_stats_clustered(m_occurrence, "ph_expo_ew"),
+  get_sensitivity_stats_clustered(m_amount, "op_expo_ew"),
+  get_sensitivity_stats_clustered(m_amount, "rg_expo_ew"),
+  get_sensitivity_stats_clustered(m_amount, "ph_expo_ew")
+) |>
+  mutate(treatment = case_when(
+    treatment == "op_expo_ew" ~ "Opportunity",
+    treatment == "rg_expo_ew" ~ "Regulatory",
+    treatment == "ph_expo_ew" ~ "Physical",
+    TRUE ~ treatment
+  ))
+  
+sensitivity_table <- sensitivity_results |>
+  select(model_type, treatment, estimate, se, t_statistic, r2yd.x, rv_q, rv_qa) |>
+  kbl(format = "latex", booktabs = TRUE, linesep = "",
+      col.names = c("Model", "Treatment", "Estimate", "SE", "t-stat", "$R^2_{Y\\sim D|\\mathbf{X}}$", "$RV_{q=1}$", "$RV_{q=1,\\alpha=0.05}$"),
+      digits = c(0, 0, 4, 4, 2, 5, 5, 5),
+      escape = FALSE,
+      caption = "Sensitivity Analysis Results") |>
+  kable_styling(latex_options = c("hold_position", "scale_down")) |>
+  add_header_above(c(" " = 2, "Statistics" = 3, "Robustness Values" = 3))
+
+sensitivity_table
+
+# Save to file
+writeLines(sensitivity_table, "results/tables/sensitivity_stats.tex")
+
+
+
+
+
+
+
+
+# Regress the treatments on the covariates
+D_on_Z_op <- feols(op_expo_ew ~ rg_expo_ew + ph_expo_ew + total_lobby_quarter + ebit + ebit_at + us_dummy | industry_year, df, vcov = ~ year + isin)
+D_on_Z_rg <- feols(rg_expo_ew ~ op_expo_ew + ph_expo_ew + total_lobby_quarter + ebit + ebit_at + us_dummy | industry_year, df, vcov = ~ year + isin)
+D_on_Z_ph <- feols(ph_expo_ew ~ op_expo_ew + rg_expo_ew + total_lobby_quarter + ebit + ebit_at + us_dummy | industry_year, df, vcov = ~ year + isin)
+
+# Get partial R2 values
+r2dxj.x_op <- partial_r2(t_statistic = tstat(D_on_Z_op)[["ebit"]], dof = df.residual(D_on_Z_op))
+r2dxj.x_rg <- partial_r2(t_statistic = tstat(D_on_Z_rg)[["ebit"]], dof = df.residual(D_on_Z_rg))
+r2dxj.x_ph <- partial_r2(t_statistic = tstat(D_on_Z_ph)[["ebit"]], dof = df.residual(D_on_Z_ph))
+
+# Get partial R2 values for outcome on treatment and covariates
+r2yxj.dx_oc <- partial_r2(t_statistic = tstat(m_occurrence)[["ebit"]], dof = df.residual(m_occurrence))
+r2yxj.dx_ex <- partial_r2(t_statistic = tstat(m_expenditure)[["ebit"]], dof = df.residual(m_expenditure))
+
+
+
+
+# Create sensitivity plots ---------------------------------------------------
+
+
+sense_op <- sensemakr(
+  model                = m_occurrence,
+  treatment            = "op_expo_ew",
+  benchmark_covariates = c("total_lobby_quarter", "ebit", "ebit_at", "us_dummy"),
+  kd                   = 10
+)
+
+sense_op$sensitivity_stats
+
+plot(sense_op)
+
+
+# Augment w/ covariates ---------------------------------------------------
+df$subs_iso3c <- unlist(lapply(df$subs_iso3c, FUN = function(x) paste(gsub("n.a.", "", unique(unlist(strsplit(x, "\n")))), collapse="|")))
+sum(df$subs_iso3c=="n.a.")
+sum(df$subs_iso3c=="")
+
+df$subs_iso3c <- gsub("NA", "", df$subs_iso3c)
+
+df$subs_iso3c[which(nchar(df$subs_iso3c)==3)] <- gsub("\\|", "", df$subs_iso3c[which(nchar(df$subs_iso3c)==3)])
+df$subs_iso3c[which(df$subs_iso3c=="")] <- NA
+
+df$country_iso_code[which(df$Firm=="NL00150002Q7")] <- "DE"
+
+df$multinational <- 0
+df$multinational[which(df$subs_iso3c != df$country_iso_code)] <- 1
+
+## If the subsidiary column isnt EXACTLY equal to the man country column, this
+## implies there is a subsidiary in another country.
+
+newdat <- read.csv("data/01_raw/boards_rep_v2/analysis_data_no_proprietary.csv", stringsAsFactors = F)
+newdat <- newdat[ , c("gvkey", "year", "cso_exists", "cdp_report")]
+df <- merge(df, newdat, by=c("gvkey", "year"), all.x=T)
+
+df$cso_exists[which(is.na(df$cso_exists) & df$year %in% c(2000:2019))] <- 0
+df$cdp_report[which(is.na(df$cdp_report) & df$year %in% c(2000:2019))] <- 0
+
+m_occurrence2 <- feols(CLI ~ op_expo_ew + rg_expo_ew + ph_expo_ew + ebit + ebit_at + us_dummy + total_lobby_quarter + cso_exists + cdp_report + n_employees + multinational | industry_year, df, vcov = ~ year + isin)
+
+summary(m_occurrence2)
+ovb_minimal_reporting(m_occurrence2, verbose = F)
+
+sense_op2 <- sensemakr(
+  model                = m_occurrence2,
+  treatment            = "op_expo_ew",
+  benchmark_covariates = c("total_lobby_quarter", "ebit", "ebit_at", "us_dummy", "cso_exists", "cdp_report", "n_employees", "multinational"),
+  kd                   = 10
+)
+
+plot(sense_op2)
+
 
 # Opportunity
 pdf("~/Dropbox (Princeton)/BBH/BBH1/results/Figures/sensitivity/sensitivity_plots_occurrence_op_qty.pdf", width = 6, height = 6)
@@ -103,8 +200,8 @@ plot(
   sensemakr(
     m_occurrence,
     treatment = "op_expo_ew",
-    benchmark_covariates = "`Total Lobbying`",
-    kd = 15
+    benchmark_covariates = "ebit",
+    kd = c(5,10,15,20,25)
   ),
   cex.label.text = 1,
   label.bump.x = 0.05,
